577c757efacc1447738afc52ed8d465484650a11
DESKTOP-FGSP1RQ\busyb <busybeaverhp@gmail.com>
2016-12-18 17:56:42 -0800

    Under Construction

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 ﻿
 var projects = "https://busybeaverhp.github.io/Projects.html";
-
+var exam145 = "https://rawgit.com/busybeaverhp/HuyPham_20161206_CSI145FinalExam/master/HuyPham_20161206_CSI145FinalExam/HuyPham_20161206_CSI145FinalExam.html"
 function topOfPage() {
     window.scrollTo(0, 0);
 }
 function loadPage(obj) {
     var page = obj;
-    document.getElementById("contentbody").innerHTML = '<object data="' + page + '" type="text/html"><embed src="' + page + '" type="text/html" /></object>';
+    document.getElementById("contentbody").innerHTML = '<object width="100%" height="2560px" background-color:white; data="' + page + '" type="text/html"><embed src="' + page + '" type="text/html" /></object>';
     return false;
 }
