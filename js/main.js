1aceaf5540e93459bd33faf6835b7ed349d3e182
DESKTOP-FGSP1RQ\busyb <busybeaverhp@gmail.com>
2016-12-18 16:44:16 -0800

    Updated Load PoC

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
-﻿function topOfPage() {
+﻿
+var projects = "https://busybeaverhp.github.io/Projects.html";
+
+function topOfPage() {
     window.scrollTo(0, 0);
+}
+function loadPage(obj) {
+    var page = obj;
+    document.getElementById("contentbody").innerHTML = '<object data="' + page + '" type="text/html"><embed src="' + page + '" type="text/html" /></object>';
+    return false;
 }
